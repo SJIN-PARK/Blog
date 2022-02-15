07ceb63d9a04e4df2f7a95e3df14250cdc945f31
SJIN-PARK <keither0627@gmail.com>
2022-02-14 19:31:13 -0800

    DB config update

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 4000;
-const db = require('./config/db');
+
+//const db = require('./config/db'); // changed db connection way
+const sequelize = require('./models').sequelize;
+sequelize.sync();
+
+app.use(express.json());
 
 app.get('/api/host', (req, res) => {
     res.send({ host : 'sungjin' });
 })
 
+/*
 app.get('/api/test', (req, res) => {
     db.query("select * from user_test", (err, data) => {
         if(!err) {
@@ -17,7 +22,9 @@ app.get('/api/test', (req, res) => {
             res.send(err);
         }
     })
-})
+}*/
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}/`);
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import styled from "styled-components";
 
 class App extends Component {
   constructor(props) {
@@ -8,6 +9,11 @@ class App extends Component {
       host : '',
     }
   }
+
+  state = {
+    query: ""
+  };
+  
 
   // componentDidMount() {
   //   this._getHost();
@@ -19,7 +25,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this._dbTest();
+    this._getHost();
   }
   
   _dbTest = async() => {
@@ -27,14 +33,59 @@ class App extends Component {
     console.log(res.data)
   }
 
+  handleInputKeyPress = event => {
+    if (event.key === "Enter") {
+      //console.log(event.target.value);
+      this.setState({
+        query: event.target.value
+      });
+      event.target.value = "";
+    }
+  };
+
+  
+
   render() {
     return(
-      <div className='App'>
-        <h3> Welcome to <u> {this.state.host} </u> Blog! </h3>
-      </div>
+      <Container query={this.state.query}>
+        <div className='App'>
+          <h3> Welcome to <u> {this.state.host} </u> Blog! </h3>
+          <Input placeholder="테마를 입력하세요" onKeyPress={this.handleInputKeyPress}></Input>
+        </div>
+      </Container>
     )
   }
 }
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+      to right,
+      rgba(20, 20, 20, 0.1) 10%,
+      rgba(20, 20, 20, 0.7) 70%,
+      rgba(20, 20, 20, 1)
+    ),
+    url(https://source.unsplash.com/random/1920x1080?${props => props.query});
+  background-size: cover;
+`;
+
+const Input = styled.input`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 190px;
+  height: 33px;
+  padding: 3px;
+  background: transparent;
+  outline: none;
+  border: none;
+  font-size: 22px;
+  color: white;
+`;
 
 export default App;
 
